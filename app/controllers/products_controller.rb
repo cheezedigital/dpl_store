c84a86a53976b3155fd30f75e381647ce45b6a52
Justin Veirs <justinveirs@gmail.com>
2014-06-12 14:22:51 -0600

    made some changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,32 @@
 class ProductsController < ApplicationController
+# this index action
+# this should list all of our products
+# index.html.erb
+  def index
+    @products = Product.all
+  end
+#this should find a particular product
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  private
+  #define a private method using strong parameters
+  def product_params
+    #params[:product]
+    params.require(:product).permit(:name)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 Product.create(name: 'DLP Shirt', price: 14.99, sku: 'dpl123', description: 'An awesome purple shirt with the DLP logo')
-Product.create(name: 'DPL Mug', price: 3.25, sku: 'dpl456', description: 'An awesome purple mig with the DPL logo')
+Product.create(name: 'DPL Mug', price: 3.25, sku: 'dpl456', description: 'An awesome purple mug with the DPL logo')
