217f59eb23cea2b636f90e32fb67bbc70aa42eb9
Justin Veirs <justinveirs@gmail.com>
2014-06-13 12:05:18 -0600

    oops. haven't commited in a bit. added a bunch of stuff. I'm a bad developer

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+
 # this index action
 # this should list all of our products
 # index.html.erb
@@ -7,7 +9,6 @@ class ProductsController < ApplicationController
   end
 #this should find a particular product
   def show
-    @product = Product.find(params[:id])
   end
 
   def new
@@ -17,10 +18,28 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
+      flash[:notice] = 'Good job! You did it.'
       redirect_to products_path
     else
+      flash[:alert] = 'Bad job! You failed.'
       render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update_attributes(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
   end
 
   private
@@ -28,5 +47,9 @@ class ProductsController < ApplicationController
   def product_params
     #params[:product]
     params.require(:product).permit(:name)
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
 
   validates :name, presence: true
+  
 
 end
